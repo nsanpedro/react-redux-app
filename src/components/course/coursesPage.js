596648396b21b2f09e55c2714d6f12b5ef38c32d
nicosp <nicolassanpedro@gmail.com>
2017-11-04 18:13:22 -0300

    inputFix

--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -1,42 +1,47 @@
+/**
+ * Created by logic on 10/12/16.
+ */
 import React, {PropTypes} from 'react';
 
-class CoursesPage extends React.Component {
-	constructor(props, context){
-		super(props, context);
+class CoursePage extends React.Component {
+  constructor(props, context){
+    super(props, context);
 
-		this.state = {
-			course: { title: "" }
-		};
+    this.state = {
+      course: {title: ""}
+    };
 
-		this.onTitleChange = this.onTitleChange.bind(this);
-		this.onClickSave = this.onClickSave.bind(this);
-	}
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
 
-	onTitleChange(event) { 
-		const course = this.state.course;
-		course.title = event.target.value;
-		this.setState = ({course: course});
-	}
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
 
-	onClickSave() {
-		alert(`Saving ${this.state.course.title}`);
-	}
+  onClickSave() {
+    alert(`Saving ` + this.state.course.title);
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
 
-	render() {
-		return(
-			<div>
-				<h1>Courses</h1>
-				<h2>Add Course</h2>
-				<input type="text"
-					   onChange={this.onTitleChange}
-					   value={this.state.course.title} />
-
-				<input type="submit"
-					   onChange={this.onClickSave} />	   
-			</div>
-		);
-	}
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
+      </div>
+    );
+  }
 }
 
-export default CoursesPage;
+export default CoursePage;
