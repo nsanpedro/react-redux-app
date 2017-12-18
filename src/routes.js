abc83c6bde95d5248e41284c00bc5405c9be2dc7
nicosp <nicolassanpedro@gmail.com>
2017-12-18 00:02:15 -0300

    courseForm

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import App from './components/app';
 import homePage from './components/home/homePage';
 import aboutPage from './components/about/aboutPage';
 import coursesPage from './components/course/coursesPage';
+import manageCoursePage from './components/course/manageCoursePage';
 
 
 
@@ -11,7 +12,8 @@ export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={homePage} />
 		<Route path="courses" component={coursesPage} />
+		<Route path="course" component={manageCoursePage} />
+		<Route path="course/:id" component={manageCoursePage} />
 		<Route path="about" component={aboutPage} />
-
 	</Route>
 );
